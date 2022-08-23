2ef60880f7c296e5b784e31d4b3a92d6b0d21a34
kmate <k_mate@inf.elte.hu>
2022-08-23 14:10:47 +0200

    JSON based configuration

--- config.h
+++ config.h
@@ -1,8 +1,40 @@
-#ifndef STASSID
-#define STASSID ""
-#define STAPSK  ""
-#endif
+#ifndef __CONFIG_H
+#define __CONFIG_H
 
-#ifndef HOSTNAME
-#define HOSTNAME "keg-scale"
+#include <ArduinoJson.h>
+#include <FS.h>
+#include <LittleFS.h>
+
+struct WiFiConfig {
+  char ssid[64];
+  char passphrase[64];
+};
+
+class Config {
+
+public:
+  char hostname[64];
+  WiFiConfig wifi;
+
+  bool load() {
+    File configFile = LittleFS.open("/config.json", "r");
+    if (!configFile) {
+      return false;
+    }
+
+    StaticJsonDocument<256> doc;
+    DeserializationError error = deserializeJson(doc, configFile);
+    if (error) {
+      return false;
+    }
+
+    strlcpy(this->hostname, doc["hostname"] | "keg-scale", sizeof(this->hostname));
+    strlcpy(this->wifi.ssid, doc["wifi"]["ssid"], sizeof(this->wifi.ssid));
+    strlcpy(this->wifi.passphrase, doc["wifi"]["passphrase"], sizeof(this->wifi.passphrase));
+
+    configFile.close();
+    return true;
+  }
+};
+
 #endif
--- keg-scale.ino
+++ keg-scale.ino
@@ -3,26 +3,49 @@
 #include <ESP8266mDNS.h>
 #include <ESP8266WiFi.h>
 #include <ESPAsyncWebServer.h>
+#include <FS.h>
+#include <LittleFS.h>
 
 #include "config.h"
 
+Config config;
 AsyncWebServer server(80);
 
-void initWiFi() {
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(STASSID, STAPSK);
-  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    Serial.println("Connection Failed! Rebooting...");
-    delay(5000);
-    ESP.restart();
-  }
-  Serial.println("Ready");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+void failSetup(const char *message) {
+  Serial.print(message);
+  Serial.println(" Rebooting...");
+  delay(5000);
+  ESP.restart();
 }
 
-void initOTA() {
-  ArduinoOTA.setHostname(HOSTNAME);
+void setupFS() {
+  if (!LittleFS.begin()) {
+    failSetup("LittleFS mount failed.");
+  }
+}
+
+void setupConfig() {
+  if (!config.load()) {
+    failSetup("Loading config failed!");
+  }
+}
+
+void setupWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(config.wifi.ssid, config.wifi.passphrase);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    failSetup("WiFi connection failed!");
+  }
+  Serial.println("WiFi connected.");
+  Serial.print("IP address: ");
+  Serial.print(WiFi.localIP());
+  Serial.println(".");
+}
+
+void setupOTA() {
+  static volatile bool updatingFS = false;
+
+  ArduinoOTA.setHostname(config.hostname);
 
   // No authentication by default
   // ArduinoOTA.setPassword("admin");
@@ -35,15 +58,20 @@ void initOTA() {
     String type;
     if (ArduinoOTA.getCommand() == U_FLASH) {
       type = "sketch";
+      updatingFS = false;
     } else { // U_FS
       type = "filesystem";
+      updatingFS = true;
+      LittleFS.end();
     }
 
-    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
     Serial.println("OTA begins for " + type);
   });
   ArduinoOTA.onEnd([]() {
     Serial.println("\nOTA finished.");
+    if (updatingFS) {
+      setupFS();
+    }
   });
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
     Serial.printf("OTA progress: %u%%\r", (progress / (total / 100)));
@@ -67,13 +95,15 @@ void initOTA() {
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Booting");
+  Serial.println("Booting...");
 
-  initWiFi();
-  initOTA();
+  setupFS();
+  setupConfig();
+  setupWiFi();
+  setupOTA();
 
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    request->send(200, "text/plain", "Hi! I am a keg scale.");
+    request->send(200, "text/plain", "Hi! I am a keg scale. SSID: " + String(config.wifi.ssid));
   });
 
   server.begin();
