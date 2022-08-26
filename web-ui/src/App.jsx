edf740ed154f3f26f8f12295b7170080e558daaf
kmate <k_mate@inf.elte.hu>
2022-08-26 21:42:40 +0200

    Add MUI with dark theme as default

--- web-ui/src/App.jsx
+++ web-ui/src/App.jsx
@@ -1,12 +1,24 @@
 import React, { Component} from "react";
 import "./App.css";
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 class App extends Component{
   render(){
     return(
-      <div className="App">
-        <h1>Hello, Keg Scale!</h1>
-      </div>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <div className="App">
+          <h1>Hello, Keg Scale!</h1>
+        </div>
+      </ThemeProvider>
     );
   }
 }
