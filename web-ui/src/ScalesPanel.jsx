f271e98b9cd8e469753d20a45a2e6a12f131f14a
kmate <k_mate@inf.elte.hu>
2022-09-03 00:13:53 +0200

    Initial UI for individual scales

--- web-ui/src/ScalesPanel.jsx
+++ web-ui/src/ScalesPanel.jsx
@@ -1,9 +1,61 @@
 import * as React from 'react';
 import useFetch from "react-fetch-hook";
+import useInterval from 'use-interval';
 import apiLocation from './apiLocation';
 
+import { Button, FormControl, FormHelperText, Input, InputAdornment, InputLabel } from '@mui/material';
 import ErrorIndicator from './ErrorIndicator';
 import LoadingIndicator from './LoadingIndicator';
+
+function ScalePanel(props) {
+  const [tick, setTick] = React.useState(false);
+  const [knownWeight, setKnownWeight] = React.useState(1000);
+
+  useInterval(
+    () => { setTick(!tick); },
+    1000 // TODO this might depend on the actual state
+  );
+
+  const { isLoading, data, error } = useFetch(apiLocation("/scale/" + props.index), { depends: [tick] });
+
+  function tare() {
+    // TODO send post request to tare scale
+    console.log("tare scale " + props.index);
+  }
+
+  function calibrate() {
+    // TODO send post request to calibrate scale
+    console.log("calibrate scale " + props.index + " with known weight " + knownWeight);
+  }
+
+  // TODO finalize UI depending on actual scale state
+  return (
+    <>
+      <p>scale {props.index}, body: {JSON.stringify(data)}</p>
+      <Button onClick={tare}>Tare</Button>
+      <FormControl variant="standard">
+          <InputLabel htmlFor="known-weight">Known weight</InputLabel>
+          <Input
+            id="known-weight"
+            value={knownWeight}
+            onChange={(event) => setKnownWeight(parseInt(event.target.value) || 0)}
+            endAdornment={<InputAdornment position="end">g</InputAdornment>}
+          />
+        </FormControl>
+      <Button onClick={calibrate}>Calibrate</Button>
+    </>
+  );
+}
+
+function IterateScalePanels(props) {
+  return (
+    <>
+      {[...Array(props.numScales).keys()].map((index) => {
+        return <ScalePanel key={"scale_" + index} index={index} />;
+      })}
+    </>
+  );
+}
 
 export default function ScalesPanel() {
 
@@ -11,7 +63,7 @@ export default function ScalesPanel() {
 
   return(
     <>
-      { isLoading ? <LoadingIndicator /> : (error ? <ErrorIndicator error={error} /> : <p>TODO: render UI for {data.numScales} scales</p>)}
+      { isLoading ? <LoadingIndicator /> : (error ? <ErrorIndicator error={error} /> : <IterateScalePanels numScales={data.numScales} />)}
     </>
   );
 }
