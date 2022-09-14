65ad2ef2a14add1cd7b16ff6fad54253d2e6fb69
kmate <k_mate@inf.elte.hu>
2022-09-14 21:21:34 +0200

    Extract TabPanel

--- web-ui/src/MainTabs.jsx
+++ web-ui/src/MainTabs.jsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import { useTheme } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -9,30 +8,9 @@ import Tab from '@mui/material/Tab';
 import ScaleIcon from '@mui/icons-material/Scale';
 import PermDeviceInformationIcon from '@mui/icons-material/PermDeviceInformation';
 
+import TabPanel from './TabPanel';
 import StatusPanel from './StatusPanel';
 import ScalesPanel from './ScalesPanel';
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      hidden={value !== index}
-      id={`main-tabs-tabpanel-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box>{children}</Box>
-      )}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-};
 
 export default function MainTabs() {
   const theme = useTheme();
@@ -40,10 +18,6 @@ export default function MainTabs() {
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
-  };
-
-  const handleChangeIndex = (index) => {
-    setValue(index);
   };
 
   return (
---
+++ web-ui/src/TabPanel.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+
+export default function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div hidden={value !== index} {...other}>
+      {value === index && (
+        <Box>{children}</Box>
+      )}
+    </div>
+  );
+}
