e6a9d77527dd98787cc76ba12bf2287bbcb86a65
kmate <k_mate@inf.elte.hu>
2023-02-07 14:43:25 +0100

    Support for toggle mode in known weight selector

--- web-ui/src/KnownWeights.jsx
+++ web-ui/src/KnownWeights.jsx
@@ -1,25 +1,33 @@
-import { Button, Divider } from '@mui/material';
+import { Button, Divider, ToggleButton, ToggleButtonGroup } from '@mui/material';
 import { Stack } from '@mui/system';
 import * as React from 'react';
 
 export default function KnownWeights(props) {
 
-  const makeClickHandler = (mass) => {
+  const [selectedLabel, setSelectedLabel] = React.useState(null);
+
+  const makeClickHandler = (mass, label) => {
     return () => {
       props.onClick(mass);
+      setSelectedLabel(label);
     }
   }
 
+  const Btn = props.isToggle ? ToggleButton : Button;
+
+  const children = (props.children || []).concat(
+    props.weights
+      .filter((weight) => weight.forTare && props.forTare || weight.forCalibration && props.forCalibration)
+      .map((weight, index) => {
+      return (
+        <Btn key={"weight_" + index} value={weight.label} onClick={makeClickHandler(weight.mass, weight.label)}>{weight.label}</Btn>
+      );
+    })
+  );
+
   return (
     <Stack divider={<Divider orientation="vertical" flexItem />} direction="row" alignItems="center" justifyContent="center">
-      {props.children}
-      {props.weights
-        .filter((weight) => weight.forTare && props.forTare || weight.forCalibration && props.forCalibration)
-        .map((weight, index) => {
-        return (
-          <Button key={"weight_" + index} onClick={makeClickHandler(weight.mass)}>{weight.label}</Button>
-        );
-      })}
+      {props.isToggle ? <ToggleButtonGroup value={selectedLabel}>{children}</ToggleButtonGroup> : children}
     </Stack>
   );
 }
