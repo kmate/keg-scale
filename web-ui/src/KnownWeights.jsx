c3d0f3cb1814c5caffb7a08db4fe63e44332d88c
kmate <k_mate@inf.elte.hu>
2023-04-15 17:04:27 +0200

    Support for selecting the first item by default in known weights

--- web-ui/src/KnownWeights.jsx
+++ web-ui/src/KnownWeights.jsx
@@ -1,10 +1,20 @@
 import { Button, Divider, ToggleButton, ToggleButtonGroup } from '@mui/material';
 import { Stack } from '@mui/system';
 import * as React from 'react';
+import { useEffect } from 'react';
 
-export default function KnownWeights({ children, isToggle, onClick, weights, forTare, forCalibration }) {
+export default function KnownWeights({ children, isToggle, selectFirst, onClick, weights, forTare, forCalibration }) {
 
-  const [selectedLabel, setSelectedLabel] = React.useState(null);
+  const showWeights = weights.filter((weight) => forTare && weight.forTare || forCalibration && weight.forCalibration);
+
+  const [selectedLabel, setSelectedLabel] = React.useState(selectFirst ? showWeights[0].label : null);
+
+  useEffect(() => {
+    if (selectFirst) {
+      console.log("clickfirst");
+      onClick(showWeights[0].mass);
+    }
+  }, [selectFirst]);
 
   const makeClickHandler = (mass, label) => {
     return () => {
@@ -16,9 +26,7 @@ export default function KnownWeights({ children, isToggle, onClick, weights, for
   const Btn = isToggle ? ToggleButton : Button;
 
   const buttons = (children || []).concat(
-    weights
-      .filter((weight) => forTare && weight.forTare || forCalibration && weight.forCalibration)
-      .map((weight, index) => {
+    showWeights.map((weight, index) => {
       return (
         <Btn key={"weight_" + index} value={weight.label} onClick={makeClickHandler(weight.mass, weight.label)}>{weight.label}</Btn>
       );
